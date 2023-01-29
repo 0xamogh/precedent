f1d994e269a6f689eda3264097deb6456da4e1ab
Amogh <amoghjahagirdargsf@gmail.com>
2023-01-29 23:18:09 +0400

    data is corrected

--- components/home/component-grid.tsx
+++ components/home/component-grid.tsx
@@ -3,19 +3,28 @@ import { useDemoModal } from "@/components/home/demo-modal";
 import Popover from "@/components/shared/popover";
 import Tooltip from "@/components/shared/tooltip";
 import { ChevronDown } from "lucide-react";
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import { motion } from "framer-motion";
+import Balancer from "react-wrap-balancer";
 
 export default function ComponentGrid() {
   const { DemoModal, setShowDemoModal } = useDemoModal();
   const [openPopover, setOpenPopover] = useState(false);
   return (
-    <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
-      <DemoModal />
+    <div className="grid-cols-1 gap-5 md:grid-cols-3">
+      {/* <DemoModal />
       <button
         onClick={() => setShowDemoModal(true)}
         className="flex w-40 items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100"
       >
         <p className="text-gray-600">Modal</p>
-      </button>
+      </button> */}
+              <motion.h1
+          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>$299.89</Balancer>
+        </motion.h1>
       <Popover
         content={
           <div className="w-full rounded-md bg-white p-2 sm:w-40">
@@ -45,11 +54,12 @@ export default function ComponentGrid() {
           />
         </button>
       </Popover>
-      <Tooltip content="Precedent is an opinionated collection of components, hooks, and utilities for your Next.js project.">
+      {/* <Tooltip content="Precedent is an opinionated collection of components, hooks, and utilities for your Next.js project.">
         <div className="flex w-40 cursor-default items-center justify-center rounded-md border border-gray-300 px-3 py-2 transition-all duration-75 hover:border-gray-800 focus:outline-none active:bg-gray-100">
           <p className="text-gray-600">Tooltip</p>
         </div>
-      </Tooltip>
+      </Tooltip> */}
     </div>
   );
 }
+
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -69,14 +69,14 @@ export default function Layout({
       </main>
       <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
         <p className="text-gray-500">
-          A free template by{" "}
+          Start dca-ing now at {" "}
           <a
             className="font-medium text-gray-800 underline transition-colors"
             href="https://twitter.com/steventey"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Steven Tey
+            dca.cash
           </a>
         </p>
       </div>
